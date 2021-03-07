cfbffb02fccf4543d01e45fe8b29403e81ed7412
Michael Pablo Gomes da Silva <MichaelPablo13@users.noreply.github.com>
2021-03-07 18:37:38 -0300

    cors police

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,8 @@ import { sequelize } from './sequelize';
 
   //CORS Should be restricted
   app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
